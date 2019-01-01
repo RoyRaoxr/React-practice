30e6d05f3706858ee2f675ad3566f05420cde0ea
Megathrone <yizhouwang418@gmail.com>
2019-01-01 17:21:59 +0800

    state

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>HI</div>
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: null
+        }
+
+        window
+            .navigator
+            .geolocation
+            .getCurrentPosition(position => this.setState({latitude: position.coords.latitude}), err => console.log(err));
+    }
+    render() {
+        return (
+            <div>
+                Latitude: {this.state.latitude}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <App/>, document.getElementById('root'));
