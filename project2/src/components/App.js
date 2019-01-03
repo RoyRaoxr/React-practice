954ff78ecfa3576b374f727d26194aaf9a4b4f8f
Megathrone <yizhouwang418@gmail.com>
2019-01-03 17:48:40 +0800

    add imagelist

--- project2/src/components/App.js
+++ project2/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 
 class App extends React.Component {
     state = {
@@ -25,7 +26,9 @@ class App extends React.Component {
                 marginTop: '10px'
             }}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length}  images
+                <h1>Found: {this.state.images.length}
+                    images</h1>
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
