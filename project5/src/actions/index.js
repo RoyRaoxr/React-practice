0ccd72736a22f3ca988adb3ecfe9c5dbe2d87e8b
Megathrone <yizhouwang418@gmail.com>
2019-01-07 17:43:15 +0800

    fi

--- project5/src/actions/index.js
+++ project5/src/actions/index.js
@@ -7,11 +7,18 @@ export const fetchPosts = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS', payload: response.data});
 }
 
-export const fetchUser = id => dispatch => {
-    _fetchUser(id,dispatch);
-}
+// export const fetchUser = id => async dispatch => {
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
 
-const _fetchUser = _.memoize(async(dispatch, id) => {
+//     dispatch({type: 'FETCH_USER', payload: response.data});
+// };
+
+// const _fetchUser = _.memoize(async (dispatch, id) => {     const response =
+// await jsonPlaceHolder.get(`/users/${id}`);     dispatch({type: 'FETCH_USER',
+// payload: response.data}); });
+export const fetchUser = id => dispatch => fetchUserLodash(id, dispatch);
+const fetchUserLodash = _.memoize(async (id, dispatch)=>{
     const response = await jsonPlaceHolder.get(`/users/${id}`);
+
     dispatch({type: 'FETCH_USER', payload: response.data});
 });
