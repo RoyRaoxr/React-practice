836d835f14b6f19a24a6381d9cb050f7d85c1f81
Megathrone <yizhouwang418@gmail.com>
2019-01-07 18:16:41 +0800

    lodash chain

--- project5/src/actions/index.js
+++ project5/src/actions/index.js
@@ -3,8 +3,15 @@ import _ from 'lodash';
 
 export const fetchPostsAndUsers = () => async(dispatch, getState) => {
     await dispatch(fetchPosts());
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(userId => dispatch(fetchUser(userId)));
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(userId => dispatch(fetchUser(userId)));
+
+    _
+        .chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 };
 
 export const fetchPosts = () => async dispatch => {
@@ -18,7 +25,4 @@ export const fetchUser = id => async dispatch => {
     dispatch({type: 'FETCH_USER', payload: response.data});
 };
 
-// export const fetchUser = id => dispatch => fetchUserLodash(id, dispatch);
-// const fetchUserLodash = _.memoize(async(id, dispatch) => {     const response
-// = await jsonPlaceHolder.get(`/users/${id}`);     dispatch({type:
-// 'FETCH_USER', payload: response.data}); });
+// Combine all
