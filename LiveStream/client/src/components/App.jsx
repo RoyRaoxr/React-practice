31fee5d979f8dc725388c75f7d08b1a9ef4933a1
Megathrone <yizhouwang418@gmail.com>
2019-01-18 17:53:10 -0500

    1

--- LiveStream/client/src/components/App.jsx
+++ LiveStream/client/src/components/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-};
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
-        </div>
+        <div></div>
       </BrowserRouter>
     </div>
   );
