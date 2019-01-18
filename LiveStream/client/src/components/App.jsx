8bccf1a05712fa2fdafda0c240b51ac40964c005
Megathrone <yizhouwang418@gmail.com>
2019-01-18 17:59:48 -0500

    1

--- LiveStream/client/src/components/App.jsx
+++ LiveStream/client/src/components/App.jsx
@@ -10,7 +10,13 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div></div>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
       </BrowserRouter>
     </div>
   );
